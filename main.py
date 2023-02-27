d5441479ea01a3317d63e28d71f81c3e13a9ac7a
William Mongan <BillJr99@users.noreply.github.com>
2023-02-27 23:03:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# https://docs.pytest.org/en/7.2.x/
+
+# content of test_sample.py
+def inc(x):
+  return x + 1
+
+if __name__ == "__main__":
+  print(inc(2))
---
+++ test_main.py
@@ -0,0 +1,13 @@
+from main import inc
+
+# https://docs.pytest.org/en/7.2.x/
+# pip install pytest
+# run with: pytest
+
+# execute with pytest
+def test_answer():
+  assert inc(3) == 5  # deliberately fails
+
+
+def test_answer2():
+  assert inc(3) == 4
