248e20e0c1091c322553d67f4a446356acb6559d
William Mongan <BillJr99@users.noreply.github.com>
2023-02-27 23:05:11 +0000

    Comment with linked git repo

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # https://docs.pytest.org/en/7.2.x/
+# https://github.com/BillJr99/GithubWorkflowPythonTestExample
+
 
 # content of test_sample.py
 def inc(x):
   return x + 1
+
 
 if __name__ == "__main__":
   print(inc(2))
